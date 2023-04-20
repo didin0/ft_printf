e4ba415cd5c3c9c02e56bb5d0217974c43f128ba
didin0 <mehdi.abbadi.pro@gmail.com>
2023-04-20 19:44:15 +0200

    init

---
+++ includes/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabbadi <mabbadi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/16 17:08:06 by mabbadi           #+#    #+#             */
+/*   Updated: 2023/04/19 13:59:38 by mabbadi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "../Libft/libft.h"
+# include <stdarg.h>
+# include <stdio.h>
+
+int	ft_printf(const char *, ...);
+
+#endif
